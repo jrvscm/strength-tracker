388156aae20585e117fcf63685d71a0a942c1f30
Chris Jarvis <jrvscm@gmail.com>
2017-11-03 18:02:54 -0600

    added files, created server/db with test get req

---
+++ server.js
@@ -0,0 +1,57 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+mongoose.Promise = global.Promise;
+
+const{PORT, DATABASE_URL} = require('./config');
+
+const app = express();
+
+app.use(morgan('common'));
+app.use(bodyParser.json());
+
+app.get('/test', (req, res) => {
+	res.json("hello-world");
+});
+
+let server;
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+	return new Promise((resolve, reject) => {
+		mongoose.connect(databaseUrl, err => {
+			if(err) {
+				return reject(err);
+			}
+			server = app.listen(port,() => {
+				console.log(`Your app is listening on port ${port}`);
+				resolve();
+			})
+			.on('error', err => {
+				mongoose.disconnect();
+				reject(err);
+			});
+		});
+	});
+}
+
+function closeServer() {
+	return mongoose.disconnect().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing server');
+			server.close(err => {
+				if(err) {
+					return reject(err);
+				}
+				resolve();
+			});
+		});
+	});
+}
+
+if (require.main === module) {
+	runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
