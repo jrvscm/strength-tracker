8d4f49e32ce17e40fe6e21fe54ccc0da69c0a5ed
Chris Jarvis <jrvscm@gmail.com>
2017-11-11 21:09:46 -0700

    add workouts/add exercises endpoints working

--- workouts/router.js
+++ workouts/router.js
@@ -21,32 +21,14 @@ router.put('/new', jsonParser,
 });
 
 //add an exercise
-router.put('/exercisess', jsonParser,
-	passport.authenticate('jwt', {session: false}),
-	(req, res) => {
-	User.findOne({'workouts._id': req.body._id})
-	.then(function(user) {
-		let workoutsList = user.workouts;
-		for(let i=0; i<workoutsList.length; i++) {
-			for(let j=0; j<workoutsList[i].length; i++){
-				console.log(workoutsList[i][j])
-			}
-		}
-		res.json(workoutsList);
-	})
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
-});
 
 router.put('/exercises', jsonParser,
 	passport.authenticate('jwt', {session: false}),
 	(req, res) => {
-		User.findOne({'workouts._id': req.body._id})
-		.then(function(user) {
-		let subWorkout = user.workouts.id("5a07a9e0d1eebc53086ee100");
-		subWorkout.exercises.push(req.body.exercise);
-		res.json(subWorkout);
-	})
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
+		User.findOneAndUpdate({username: req.body.username, 'workouts._id': req.body._id},
+			{$push: {'workouts.$.exercises': req.body.exercise}, new: true})
+		.then(user => res.json(user.apiRepr()))
+    	.catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
 
