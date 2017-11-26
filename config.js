636cfabad73a417342986a6cf1003f17d822aa71
Chris Jarvis <jrvscm@gmail.com>
2017-11-26 11:37:22 -0700

    working on mobile styles

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ exports.DATABASE_URL =
 exports.TEST_DATABASE_URL = 
 	process.env.TEST_DATABASE_URL ||
 	global.TEST_DATABASE_URL ||
-	'mongodb://localhost/strength-tracker';				   
+	'mongodb://localhost/strength-tracker-test';				   
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
