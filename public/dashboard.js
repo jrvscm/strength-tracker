5cbf39ce47c7540d31205eeec1733e3886f7ca0e
Chris Jarvis <jrvscm@gmail.com>
2017-11-28 22:25:37 -0700

    testing localStorage iOS

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,20 +1,20 @@
 function watchViewProgressBtn() {
 	$('#workout-choices-list').on('click', '#view-progress-button', event => {
-		window.location.href = '/myprogress.html';
+		//window.location.href = '/myprogress.html';
 		getUserId();
 	});
 }
 
 function watchMyWorkoutsBtn() {
 	$('#workout-choices-list').on('click', '#view-all-workouts-button', event=> {
-		window.location.href = '/myworkouts.html';
+		//window.location.href = '/myworkouts.html';
 		getUserId();
 	});
 }
 
 function watchCreateWorkoutBtn() {
 	$('#workout-choices-list').on('click', '#create-workout-button', event => {
-		window.location.href = '/createworkout.html';
+		//window.location.href = '/createworkout.html';
 		getUserId();
 	});
 }
